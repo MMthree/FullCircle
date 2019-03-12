496523ec15a6c2c2800e47f5a1ba6cf36ff1ab81
Mario <mmisthree@gmail.com>
2019-03-11 23:42:39 -0700

    heroku fixes

--- server.js
+++ server.js
@@ -16,13 +16,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: process.env.PASS_SECRET, resave: true, saveUninitialized: true }));
+app.use(session({ secret: process.env.SECRET, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
